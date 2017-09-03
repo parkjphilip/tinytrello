2da2b16052b902c35ca087169ea425071c18e3f0
Philip <park.j.philip@gmail.com>
2017-09-03 13:51:43 -0400

    add list model

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,13 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   todos: [],
-
+  lists: [],
+  
   actions: {
     addTodo: function() {
       debugger
       this.set('addTodo', true);
     },
     saveTodo: function() {
+      var todo = this.get('newTodo');
+      this.get('todos').pushObject(todo);
+      this.send('cancel';)
+    }
+    cancel: function() {
       this.set('addTodo', false);
     }
   }
